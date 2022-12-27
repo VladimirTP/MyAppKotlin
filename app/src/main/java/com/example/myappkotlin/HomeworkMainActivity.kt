81c5afd3c637973c60d6ad1d9a9f0a7a4c5f0035
TamashovVladimir <v.tamashov@gmail.com>
2022-12-27 18:05:26 +0300

    Homework_ActivitySwitch

---
+++ app/src/main/java/com/example/myappkotlin/HomeworkMainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myappkotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeworkMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homework_main)
+
+    }
+    override fun onBackPressed() {
+        val randomIntent = Intent(this,HomeworkSecondActivity::class.java)
+        randomIntent.putExtra("GREETINGS", "Hi from MainActivity")
+        startActivity(randomIntent)
+
+    }
+}
---
+++ app/src/main/java/com/example/myappkotlin/HomeworkSecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myappkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class HomeworkSecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homework_second)
+
+        val textFromMainActivity=intent.getStringExtra("GREETINGS")
+        val textView = findViewById<TextView>(R.id.textViewHomework)
+        textView.text=textFromMainActivity
+
+    }
+}
